ff79281b725cd30a4e415d68569ea7cac164a82c
henhecta <yn.yoshida.o.a524287@gmail.com>
2018-08-01 20:13:15 +0900

    test

--- main.js
+++ main.js
@@ -363,7 +363,7 @@ function init() {
         InputOpe();
         world.Step(1 / 60, 5, 5);
         for (var i = 0; i < BodyNum; i++) {
-            //Bodies[i].grap.setTransformationMatrix(1, 0, 0, 1, 0, 0);
+            Bodies[i].grap.setTransformationMatrix(1, 0, 0, 1, 0, 0);
             Bodies[i].grap.setPosition((Bodies[i].b2body.GetPosition().x - Bodies[i].width / 2) * scale, (Bodies[i].b2body.GetPosition().y - Bodies[i].height / 2) * scale);
             Bodies[i].grap.setRotationByAnchor(Bodies[i].b2body.GetAngle() / Math.PI * 180, "center");
         }
